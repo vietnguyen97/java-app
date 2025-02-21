5d57c77d8baf395098c8cae6ed0b02243fac015e
nguyen97 <tvietnguyen1997@gmail.com>
2025-02-21 13:57:27 +0700

    feat: auth JWT

--- src/main/java/com/devteria/identity_service/controller/AuthenticationController.java
+++ src/main/java/com/devteria/identity_service/controller/AuthenticationController.java
@@ -2,8 +2,11 @@ package com.devteria.identity_service.controller;
 
 import com.devteria.identity_service.dto.request.ApiResponse;
 import com.devteria.identity_service.dto.request.AuthenticationRequest;
+import com.devteria.identity_service.dto.request.IntrospectRequest;
 import com.devteria.identity_service.dto.response.AuthenticationResponse;
+import com.devteria.identity_service.dto.response.IntrospectResponse;
 import com.devteria.identity_service.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+
 @RestController
 @RequestMapping( "/auth")
 @RequiredArgsConstructor
@@ -19,13 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
-    @PostMapping("log-in")
+    @PostMapping("/token")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        boolean data = authenticationService.authenticate(request);
+        var data = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
-                .data(AuthenticationResponse.builder()
-                        .authenticated(data)
-                        .build())
+                .data(data)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var data = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .data(data)
                 .build();
     }
 }
--- src/main/java/com/devteria/identity_service/service/AuthenticationService.java
+++ src/main/java/com/devteria/identity_service/service/AuthenticationService.java
@@ -1,23 +1,79 @@
 package com.devteria.identity_service.service;
 
 import com.devteria.identity_service.dto.request.AuthenticationRequest;
+import com.devteria.identity_service.dto.request.IntrospectRequest;
+import com.devteria.identity_service.dto.response.AuthenticationResponse;
+import com.devteria.identity_service.dto.response.IntrospectResponse;
 import com.devteria.identity_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
     UserRepository userRepository;
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    protected String SECRET;
 
-    public boolean authenticate(AuthenticationRequest request) {
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new RuntimeException("User not found"));
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!authenticated) {
+            throw new RuntimeException("Invalid credentials");
+        }
+        var token = generateToken(user.getUsername());
+        return AuthenticationResponse.builder().token(token).authenticated(true).build();
+    }
+
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(SECRET.getBytes());
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expityTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+        var verifyed = signedJWT.verify(verifier);
+        return IntrospectResponse.builder().valid(verifyed && expityTime.after(new Date())).build();
+    }
+
+    private String generateToken(String username) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        JWTClaimsSet jwtClaimSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("identity-service")
+                .issueTime(new java.util.Date())
+                .expirationTime(new java.util.Date(new java.util.Date().getTime() + 1000 * 60 * 60 * 10))
+                .build();
+        Payload payload = new Payload(jwtClaimSet.toJSONObject());
+        JWSObject jwsObject = new JWSObject(header, payload);
+        try {
+            jwsObject.sign(new MACSigner(SECRET.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Can not created token");
+            throw new RuntimeException(e);
+        }
     }
 }

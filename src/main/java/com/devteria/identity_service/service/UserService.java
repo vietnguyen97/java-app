e9f1271576a80bff7e17d4e7fa261bbc97002f5d
nguyen97 <tvietnguyen1997@gmail.com>
2025-03-08 16:11:12 +0700

    feat: unique username

--- src/main/java/com/devteria/identity_service/service/UserService.java
+++ src/main/java/com/devteria/identity_service/service/UserService.java
@@ -3,8 +3,10 @@ package com.devteria.identity_service.service;
 import java.util.HashSet;
 import java.util.List;
 import com.devteria.identity_service.enums.Role;
+import com.devteria.identity_service.exception.AppException;
 import com.devteria.identity_service.repository.RoleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,18 +31,17 @@ public class UserService {
 	PasswordEncoder passwordEncoder;
 
 	public UserResponse createdUser(UserCreatedRequest request) {
-		if (useRepository.existsByUsername(request.getUsername()))
-			throw new RuntimeException("User existed");
-
 		User user = userMapper.toUser(request);
 		System.out.println("Request received: " + request);
 
 		HashSet<String> roles = new HashSet<>();
 		roles.add(Role.USER.name());
-//		user.setRoles(roles);
 		user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-		user = useRepository.save(user);
+		try {
+			user = useRepository.save(user);
+		} catch (DataIntegrityViolationException e) {
+			throw new RuntimeException("User exists");
+		}
 		return userMapper.toUserResponse(user);
 	}
 
